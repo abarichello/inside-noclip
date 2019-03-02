2cdba374743da3b67c23bccd626a40a9dc4c1a4c
BARICHELLO <artur@barichello.me>
2019-03-02 18:45:05 -0300

    Add log function

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # inside-noclip
 NOCLIP Cheat Engine table with Lua scripts for Playdead's INSIDE game.
 
+# Video
+A video using this cheat table:
+- soon
+
 # Usage
 1. Download Cheat Engine 6.8.3+
 2. Open the game.
@@ -13,6 +17,6 @@ NOCLIP Cheat Engine table with Lua scripts for Playdead's INSIDE game.
 SOON tm.
 
 # Reference
-[Auto Assembler](https://wiki.cheatengine.org/index.php?title=Cheat_Engine:Auto_Assembler)
-[celua.txt](https://github.com/cheat-engine/cheat-engine/blob/master/Cheat%20Engine/bin/celua.txt)
-[Stephen Chapman's YT channel](https://www.youtube.com/watch?v=eEg3q2qocwQ)
+- [Auto Assembler](https://wiki.cheatengine.org/index.php?title=Cheat_Engine:Auto_Assembler)
+- [celua.txt](https://github.com/cheat-engine/cheat-engine/blob/master/Cheat%20Engine/bin/celua.txt)
+- [Stephen Chapman's YT channel](https://www.youtube.com/watch?v=eEg3q2qocwQ)
--- asm.lua
+++ asm.lua
@@ -1,10 +1,14 @@
 local asm = {}
 
--- Open Cheat Engine's auto assemble(*.asm) scripts
+function log(message)
+    print("--> asm.lua: " .. message)
+end
+
+-- Open Cheat Engine's auto assemble(*.asm) scripts and returns contents as a string
 function asm.open(path)
     local file = io.open(path, "r")
     if file == nil then
-        print("--> asm.lua: Couldn't find file!")
+        log("Couldn't find file!")
         return
     end
     local contents = file:read("*all")
@@ -14,13 +18,13 @@ end
 -- Allocs memory inside the process, registers necessary user defined symbols
 -- and executes the assembly script to create the code cave.
 function asm.enable(info)
-    print("--> asm.lua: Enabling script " .. info.asmPath)
+    log("Enabling script " .. info.asmPath)
     unregisterSymbol(info.memSymbol)
     unregisterSymbol(info.addressSymbol)
 
     registerSymbol(info.addressSymbol, info.address)
-    mem = allocateMemory(0x1000) -- 4KB
-    registerSymbol(info.memSymbol, mem)
+    memAddress = allocateMemory(0x1000) -- 4KB
+    registerSymbol(info.memSymbol, memAddress)
 
     autoAssemble(asm.open(info.asmPath))
 end
@@ -28,11 +32,11 @@ end
 -- Unregisters all user defined symbols on enable.
 -- Frees memory used in code cave.
 function asm.disable(info)
-    print("--> asm.lua: Disabling script")
+    log("Disabling script")
     writeBytes(info.address, info.bytes)
     unregisterSymbol(info.memSymbol)
     unregisterSymbol(info.addressSymbol)
-    deAlloc(mem, 0x1000) -- 4KB
+    deAlloc(memAddress, 0x1000) -- 4KB
 end
 
 return asm
